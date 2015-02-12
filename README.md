6295ce54d036f4488dd83ec071d4cc47fbfb0ca6
Benjamin Bachmann <bbachmann@bloopark.de>
2015-02-12 10:04:11 +0100

    add more desctiptions and the option to translate the blocks

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Building Block for Shop Category
 ================================
 
-- enables Building Blocks for Shop Category in the odoo ecommerce shop frontend
+- This Add-on enables Building Blocks for Shop Category in the odoo ecommerce shop frontend
--- models/product.py
+++ models/product.py
@@ -5,13 +5,16 @@ from openerp import fields
 class product_public_category(Model):
     _inherit = 'product.public.category'
 
-
+    # Fields for the different Block injections
     website_description_1 = fields.Html(
-        string='Description 1 for the website'
+        string='Description 1 for the website',
+        translate=True
     )
     website_description_2 = fields.Html(
-        string='Description 2 for the website'
+        string='Description 2 for the website',
+        translate=True
     )
     website_description_3 = fields.Html(
-        string='Description 3 for the website'
+        string='Description 3 for the website',
+        translate=True
     )
